e652bb8a17f55ec38cc1aa78c349f1a52377caae
Anders Vestergaard <a.vestergaard@uber.com>
2025-07-21 15:34:11 +0200

    make counter work offline

--- src/feature/counter/index.tsx
+++ src/feature/counter/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { DateTime } from 'luxon'
 import classes from './index.module.css'
 import { useLocalStorage } from '../../components/hooks/use-local-storage'
 
@@ -6,6 +7,11 @@ export function Counter() {
   const [sumStr, setSumStr] = useLocalStorage({ key: 'sum', defaultValue: '0' })
   const invested = 200_000
   const increment = (invested * 0.07) / 365 / 24 / 60 / 60 / timesPerSecond
+
+  React.useEffect(() => {
+    const secSinceStart = calculateSecondsSinceNoonWhenStart()
+    setSumStr((increment * secSinceStart).toFixed(10))
+  }, [])
 
   React.useEffect(() => {
     const interval = setInterval(() => {
@@ -20,6 +26,13 @@ export function Counter() {
       <div className={classes.numberDisplay}>{sumStr}</div>
     </div>
   )
+}
+
+function calculateSecondsSinceNoonWhenStart() {
+  const start = DateTime.fromISO('2025-07-21T12:00:00Z')
+  const now = DateTime.now()
+  const diff = now.diff(start, 'seconds').seconds
+  return Math.max(0, diff)
 }
 
 const timesPerSecond = 2
